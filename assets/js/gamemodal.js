82f55ad53dfb2c862d4bb256a142e87e5850940d
Bram ten Cate <agj.tencate@gmail.com>
2019-08-29 00:01:09 +0200

    #00000|Portfolio|various ui fixes

--- assets/js/gamemodal.js
+++ assets/js/gamemodal.js
@@ -1,27 +1,39 @@
-// Get the modal
 var modal = document.getElementById("myModal");
 
-modal.onclick = function(){
-  document.getElementById('inlineFrame').src += '';
-  modal.style.display = "none";
-  $("body").css("overflow", "auto");
-}
+//modal.onclick = function(){
+//  document.getElementById('inlineFrame').src += '';
+//  modal.style.display = "none";
+//  $("body").css("overflow", "auto");
+//}
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("main_img");
-var modalImg = document.getElementById("inlineFrame");
-var captionText = document.getElementById("caption");
+let img = document.getElementById("main_img");
+let modalImg = document.getElementById("inlineFrame");
 img.onclick = function(){
+  setPosition();
   modal.style.display = "block";
   $("body").css("overflow", "hidden");
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
-  document.getElementById('inlineFrame').src += '';
+  $('#inlineFrame').attr('src', $('#inlineFrame').attr('src'));
   modal.style.display = "none";
   $("body").css("overflow", "auto");
 }
+
+function setPosition(){
+  let w = document.documentElement.clientWidth;
+  let h = document.documentElement.clientHeight;
+  
+  let x = (w / 2) - (1441.2 / 2);
+  let y = (h / 2) - (921.2 / 2);
+  
+  if (x < 0) x = 0; 
+  if (y < 0) y = 0; 
+  
+  document.getElementById("inlineFrame").style.left = x + "px";
+  document.getElementById("inlineFrame").style.top = y + "px";
+}
+
+window.addEventListener("resize", setPosition);
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,9 +6,15 @@ var sticky = navbar.offsetTop;
 function move() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
+	document.getElementById("nav").style.margin = "auto";
+	document.getElementById("nav").style.zIndex = "3";
+	document.getElementById("nav").style.position = "fixed";
 	$("nav").css('background', 'rgba(42, 41, 68, 1)');
   } else {
     navbar.classList.remove("sticky");
+	document.getElementById("nav").style.margin = "-4rem auto 0 auto";
+	document.getElementById("nav").style.zIndex = "2";
+	document.getElementById("nav").style.position = "relative";
     $("nav").css('background', 'rgba(255, 255, 255, 0.175)');
   }
 }
