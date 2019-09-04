bc4010f7651e5a37fba6aa566aba3a2ea1d967ed
Bram ten Cate <agj.tencate@gmail.com>
2019-09-04 21:24:01 +0200

    #00000|Portfolio|responsive fixes

--- assets/js/goto_header.js
+++ assets/js/goto_header.js
@@ -1,3 +1,4 @@
 function gotoHeader() {
 	window.scrollTo(0, $(window).scrollTop() - -document.getElementById("header").getBoundingClientRect().top);
+	document.getElementById("bod").classList.remove("is-navPanel-visible");
 }
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -4,17 +4,20 @@ var navbar = document.getElementById("nav");
 var sticky = navbar.offsetTop;
 
 function move() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-	document.getElementById("nav").style.margin = "auto";
-	document.getElementById("nav").style.zIndex = "3";
-	document.getElementById("nav").style.position = "fixed";
-	$("nav").css('background', 'rgba(42, 41, 68, 1)');
-  } else {
-    navbar.classList.remove("sticky");
-	document.getElementById("nav").style.margin = "-4rem auto 0 auto";
-	document.getElementById("nav").style.zIndex = "2";
-	document.getElementById("nav").style.position = "relative";
-    $("nav").css('background', 'rgba(255, 255, 255, 0.175)');
-  }
+		navbar.classList.remove("sticky");
+		document.getElementById("nav").style.margin = "-4rem auto 0 auto";
+		document.getElementById("nav").style.zIndex = "2";
+		document.getElementById("nav").style.position = "relative";
+		$("nav").css('background', 'rgba(255, 255, 255, 0.175)');
+		
+  if (window.innerWidth > 736) {
+	  if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+		document.getElementById("nav").style.margin = "auto";
+		document.getElementById("nav").style.zIndex = "3";
+		document.getElementById("nav").style.position = "fixed";
+		$("nav").css('background', 'rgba(42, 41, 68, 1)');
+		$("nav").css('background', 'rgba(42, 34, 60, 1)');
+	  } 
+  }	  
 }
